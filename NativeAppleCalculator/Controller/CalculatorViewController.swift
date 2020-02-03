2d0e4a893b1ba5f1daa0cfeff5fa92ad5b0592b0
Igor Shelginskiy <engwar@yandex.ru>
2020-02-03 18:59:36 +0300

    install SnapKit

---
+++ NativeAppleCalculator/Controller/CalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorViewController.swift
+//  NativeAppleCalculator
+//
+//  Created by Igor Shelginskiy on 2/3/20.
+//  Copyright © 2020 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
